c0a11c3631b275c86b218504357e41314337e507
newcowboy <yzf851@sohu.com>
2017-04-24 21:36:21 +0800

    modify out/basic to res/basic

--- midc_dc-web/src/main/java/org/cw/midc/controller/out/oauth/DeviceTypeOAuthController.java
+++ midc_dc-web/src/main/java/org/cw/midc/controller/out/oauth/DeviceTypeOAuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/out/basic")
+@RequestMapping("/res/basic")
 public class DeviceTypeOAuthController {
 	
 	@Autowired
--- midc_dc-web/src/main/java/org/cw/midc/controller/out/oauth/HeartBeatOAuthController.java
+++ midc_dc-web/src/main/java/org/cw/midc/controller/out/oauth/HeartBeatOAuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
-@RequestMapping("out/basic")
+@RequestMapping("res/basic")
 public class HeartBeatOAuthController {
 	
 	@Autowired
