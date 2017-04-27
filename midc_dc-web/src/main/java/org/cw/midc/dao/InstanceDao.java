8146439dbb5f122dbbb13c348b4cdf093c8d5d87
newcowboy <yzf851@sohu.com>
2017-04-27 15:43:14 +0800

    to mybatis

---
+++ midc_dc-web/src/main/java/org/cw/midc/dao/InstanceDao.java
@@ -0,0 +1,9 @@
+package org.cw.midc.dao;
+
+import org.cw.midc.entity.Instance;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InstanceDao extends BaseDao<Instance> {
+
+}
---
+++ midc_dc-web/src/main/java/org/cw/midc/dao/RisPacsRelDao.java
@@ -0,0 +1,9 @@
+package org.cw.midc.dao;
+
+import org.cw.midc.entity.RisPacsRel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RisPacsRelDao extends BaseDao<RisPacsRel> {
+
+}
