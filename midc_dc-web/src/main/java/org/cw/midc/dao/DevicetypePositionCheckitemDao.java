92b257760261e659c8d9b3db6f7033e3764f2a59
lnshjyh1235 <asitf@126.com>
2017-05-02 01:28:41 +0800

    modify bug

--- midc_dc-web/src/main/java/org/cw/midc/dao/DevicetypePositionCheckitemDao.java
+++ midc_dc-web/src/main/java/org/cw/midc/dao/DevicetypePositionCheckitemDao.java
@@ -1,7 +1,9 @@
 package org.cw.midc.dao;
 
 import org.cw.midc.entity.DevicetypePositionCheckitem;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class DevicetypePositionCheckitemDao extends BaseDao<DevicetypePositionCheckitem> {
 
 }
